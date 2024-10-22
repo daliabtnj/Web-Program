c5f755a1eb4a151a8d5be376f75889de16ae1494
victorrf <victorrf2002@gmail.com>
2024-10-21 22:18:50 -0400

    Implement local storage to store client

--- javascript/client.js
+++ javascript/client.js
@@ -1,13 +1,25 @@
 // Sign-Up Form Validation
 
 // Sign-Up Page required information
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const phone = document.getElementById('phone');
-const password = document.getElementById('password');
-const password_repeat = document.getElementById('password-repeat');
+const name = document.getElementById('name').value;
+const email = document.getElementById('email').value;
+const phone = document.getElementById('phone').value;
+const password = document.getElementById('password').value;
+const password_repeat = document.getElementById('password-repeat').value;
 const form = document.getElementById('form');
 const errors = document.getElementById('error');
+
+let client = {
+    'name': name,
+    'email': email,
+    'phone': phone,
+    'password': password,
+};
+
+// let client_arr = [];
+
+
+
 
 // Error Handling and Form Validation
 form.addEventListener('submit', (e) => {
@@ -16,7 +28,7 @@ form.addEventListener('submit', (e) => {
     errors.innerText = '';
 
     // If the password does not match the password confirmation, throw error
-    if (password_repeat.value != password.value) {
+    if (password_repeat != password) {
         errorMessages.push('Passwords do not match.');
     }
 
@@ -25,8 +37,19 @@ form.addEventListener('submit', (e) => {
         errors.innerText = errorMessages.join(', ')
     }
     else {
+        // client_arr.push(client);
+
+        // client = JSON.stringify(client);
+
+        localStorage.setItem("client", JSON.stringify(client))
+
+        let newClient = localStorage.getItem("client");
+
+
         console.log("Form is valid. Redirecting...");
         window.location.href = "customer-dashboard.html";   // redirect to customer dashboard
+        console.log(JSON.parse(newClient));
     }
 
 });
+
