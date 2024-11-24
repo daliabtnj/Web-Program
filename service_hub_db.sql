560162a699f2796fdcabc25c1d64ee85adbaae50
Dalia <dalia.betinjaneh03@gmail.com>
2024-11-23 22:51:12 -0500

    update db

--- service_hub_db.sql
+++ service_hub_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 23, 2024 at 02:03 AM
+-- Generation Time: Nov 24, 2024 at 04:37 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -48,6 +48,28 @@ CREATE TABLE `Bills` (
   `STATUS` enum('unpaid','paid') DEFAULT 'unpaid',
   `DATE` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `BusinessSettings`
+--
+
+CREATE TABLE `BusinessSettings` (
+  `id` int(11) NOT NULL,
+  `company_name` varchar(255) NOT NULL,
+  `logo` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `BusinessSettings`
+--
+
+INSERT INTO `BusinessSettings` (`id`, `company_name`, `logo`, `address`, `email`, `phone`) VALUES
+(1, 'CompanyName', '/uploads/1732418636949-49163177.png', '222 Maplewood Street, H5H 1A8, Montreal, QC', 'email@servicehub.com', '+1(514)123-1117');
 
 -- --------------------------------------------------------
 
@@ -100,8 +122,7 @@ INSERT INTO `Services` (`id`, `service_name`, `description`, `default_price`) VA
 (3, 'Third Service - Pet Grooming', 'Our pet grooming service ensures your furry friends look and feel their best. We offer comprehensive grooming services including bathing, haircuts, and nail trimming.', 50),
 (4, 'Other Service - Private Trainer', 'Our private training service offers personalized fitness coaching tailored to your individual goals. Whether you’re looking to improve your strength, endurance, or overall health, our trainers are here to guide you.', 35),
 (5, 'Last Service - Stylist', 'Our stylist service offers expert advice and assistance in makeup, hair, and clothing selection for special events. Whether you’re preparing for a wedding, party, or business event, our professional stylists will help you look your best.', 175),
-(6, 'Test Service', 'Private lessons for kids', 500),
-(7, 'dd', 'test ', 999);
+(6, 'Test Service', 'Private lessons for kids less than 10 years old', 500);
 
 --
 -- Indexes for dumped tables
@@ -121,6 +142,12 @@ ALTER TABLE `Bills`
   ADD PRIMARY KEY (`id`),
   ADD KEY `service_request_id` (`service_request_id`),
   ADD KEY `client_id` (`client_id`);
+
+--
+-- Indexes for table `BusinessSettings`
+--
+ALTER TABLE `BusinessSettings`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `Clients`
@@ -161,6 +188,12 @@ ALTER TABLE `Bills`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `BusinessSettings`
+--
+ALTER TABLE `BusinessSettings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `Clients`
 --
 ALTER TABLE `Clients`
@@ -186,15 +219,15 @@ ALTER TABLE `Services`
 -- Constraints for table `Bills`
 --
 ALTER TABLE `Bills`
-  ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`service_request_id`) REFERENCES `ServiceRequests` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `bills_ibfk_2` FOREIGN KEY (`client_id`) REFERENCES `Clients` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`service_request_id`) REFERENCES `servicerequests` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `bills_ibfk_2` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `ServiceRequests`
 --
 ALTER TABLE `ServiceRequests`
-  ADD CONSTRAINT `servicerequests_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `Clients` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `servicerequests_ibfk_2` FOREIGN KEY (`service_id`) REFERENCES `Services` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `servicerequests_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `servicerequests_ibfk_2` FOREIGN KEY (`service_id`) REFERENCES `services` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
